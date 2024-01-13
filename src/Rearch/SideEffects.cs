2683271b9e44e3315f596abe3adcd2ffe4ed00fd
Nathaniel Bond <nabond251@gmail.com>
2024-01-13 09:54:11 -0500

    Implement state side effects

--- src/Rearch/SideEffects.cs
+++ src/Rearch/SideEffects.cs
@@ -7,6 +7,33 @@ namespace Rearch;
 public static class BuiltinSideEffectExtensions
 {
     /// <summary>
+    /// Side effect that provides a way for capsules to contain some state,
+    /// where the initial state is computationally expensive.
+    /// Similar to the `useState` hook from React;
+    /// see https://react.dev/reference/react/useState.
+    /// </summary>
+    public static (T, Action<T>) LazyState<T>(
+        this ISideEffectRegistrar registrar,
+        Func<T> init)
+    {
+        var (getter, setter) = registrar.Register(api =>
+        {
+            var state = init();
+
+            T Getter() => state;
+            void Setter(T newState)
+            {
+                state = newState;
+                api.Rebuild();
+            }
+
+            return Tuple.Create(Getter, Setter);
+        });
+
+        return (getter(), setter);
+    }
+
+    /// <summary>
     /// Side effect that provides a way for capsules to contain some state.
     /// Similar to the `useState` hook from React;
     /// see https://react.dev/reference/react/useState.
@@ -14,5 +41,5 @@ public static class BuiltinSideEffectExtensions
     public static (T, Action<T>) State<T>(
         this ISideEffectRegistrar registrar,
         T initial) =>
-        (default, _ => { });
+        registrar.LazyState(() => initial);
 }
