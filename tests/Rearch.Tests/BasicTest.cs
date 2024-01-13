9b13478e5091b47a337fd7def099db67f0763ff7
Nathaniel Bond <nabond251@gmail.com>
2024-01-13 14:17:22 -0500

    Test dep graph

--- tests/Rearch.Tests/BasicTest.cs
+++ tests/Rearch.Tests/BasicTest.cs
@@ -3,6 +3,7 @@
 // </copyright>
 
 using Newtonsoft.Json.Bson;
+using System.Runtime.Intrinsics.X86;
 
 namespace Rearch.Tests;
 
@@ -130,5 +131,180 @@ public class BasicTest
         SetState(8);
 
         Assert.Equal([1, 2, 3, 5, 6, 7], states);
+    }
+
+    // We use a more sophisticated graph here for a more thorough
+    // test of all functionality
+    //
+    // -> A -> B -> C -> D
+    //      \      / \
+    //  H -> E -> F -> G
+    //
+    // C, D, E, G, H are super pure. A, B, F are not.
+    [Fact]
+    public void ComplexDependencyGraph()
+    {
+        Dictionary<Func<ICapsuleHandle, object?>, int> builds = [];
+
+        object? A(ICapsuleHandle use)
+        {
+            if (!builds.ContainsKey(A))
+            {
+                builds[A] = 1;
+            }
+            else
+            {
+                builds[A]++;
+            }
+
+            return use.State(0);
+        }
+
+        object? B(ICapsuleHandle use)
+        {
+            if (!builds.ContainsKey(B))
+            {
+                builds[B] = 1;
+            }
+            else
+            {
+                builds[B]++;
+            }
+
+            use.Register(_ => new object());
+            return (((int, Action<int>))use.Call(A)).Item1;
+        }
+
+        object? H(ICapsuleHandle use)
+        {
+            if (!builds.ContainsKey(H))
+            {
+                builds[H] = 1;
+            }
+            else
+            {
+                builds[H]++;
+            }
+
+            return 1;
+        }
+
+        object? E(ICapsuleHandle use)
+        {
+            if (!builds.ContainsKey(E))
+            {
+                builds[E] = 1;
+            }
+            else
+            {
+                builds[E]++;
+            }
+
+            return (((int, Action<int>))use.Call(A)).Item1 + (int)use.Call(H);
+        }
+
+        object? F(ICapsuleHandle use)
+        {
+            if (!builds.ContainsKey(F))
+            {
+                builds[F] = 1;
+            }
+            else
+            {
+                builds[F]++;
+            }
+
+            use.Register(_ => new object());
+            return use.Call(E);
+        }
+
+        object? C(ICapsuleHandle use)
+        {
+            if (!builds.ContainsKey(C))
+            {
+                builds[C] = 1;
+            }
+            else
+            {
+                builds[C]++;
+            }
+
+            return (int)use.Call(B) + (int)use.Call(F);
+        }
+
+        object? D(ICapsuleHandle use)
+        {
+            if (!builds.ContainsKey(D))
+            {
+                builds[D] = 1;
+            }
+            else
+            {
+                builds[D]++;
+            }
+
+            return use.Call(C);
+        }
+
+        object? G(ICapsuleHandle use)
+        {
+            if (!builds.ContainsKey(G))
+            {
+                builds[G] = 1;
+            }
+            else
+            {
+                builds[G]++;
+            }
+
+            return (int)use.Call(C) + (int)use.Call(F);
+        }
+
+        using var container = new Container();
+        Assert.True(builds.Count == 0);
+
+        Assert.Equal(1, (int)container.Read(D));
+        Assert.Equal(2, (int)container.Read(G));
+        Assert.Equal(1, builds[A]);
+        Assert.Equal(1, builds[B]);
+        Assert.Equal(1, builds[C]);
+        Assert.Equal(1, builds[D]);
+        Assert.Equal(1, builds[E]);
+        Assert.Equal(1, builds[F]);
+        Assert.Equal(1, builds[G]);
+        Assert.Equal(1, builds[H]);
+
+        (((int, Action<int>))container.Read(A)).Item2(0);
+        Assert.Equal(1, (int)container.Read(D));
+        Assert.Equal(2, (int)container.Read(G));
+        Assert.Equal(2, builds[A]);
+        Assert.Equal(1, builds[B]);
+        Assert.Equal(1, builds[C]);
+        Assert.Equal(1, builds[D]);
+        Assert.Equal(1, builds[E]);
+        Assert.Equal(1, builds[F]);
+        Assert.Equal(1, builds[G]);
+        Assert.Equal(1, builds[H]);
+
+        (((int, Action<int>))container.Read(A)).Item2(1);
+        Assert.Equal(3, builds[A]);
+        Assert.Equal(2, builds[B]);
+        Assert.Equal(1, builds[C]);
+        Assert.Equal(1, builds[D]);
+        Assert.Equal(2, builds[E]);
+        Assert.Equal(2, builds[F]);
+        Assert.Equal(1, builds[G]);
+        Assert.Equal(1, builds[H]);
+
+        Assert.Equal(3, (int)container.Read(D));
+        Assert.Equal(5, (int)container.Read(G));
+        Assert.Equal(3, builds[A]);
+        Assert.Equal(2, builds[B]);
+        Assert.Equal(2, builds[C]);
+        Assert.Equal(2, builds[D]);
+        Assert.Equal(2, builds[E]);
+        Assert.Equal(2, builds[F]);
+        Assert.Equal(2, builds[G]);
+        Assert.Equal(1, builds[H]);
     }
 }
