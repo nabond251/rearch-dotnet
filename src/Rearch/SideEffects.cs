37d044f6eafbab3eeb37991d5aa0733360db211b
Nathaniel Bond <nabond251@gmail.com>
2024-01-13 09:45:00 -0500

    Test state updates

---
+++ src/Rearch/SideEffects.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="SideEffects.cs" company="SdgApps">
+// Copyright (c) SdgApps. All rights reserved.
+// </copyright>
+
+namespace Rearch;
+
+public static class BuiltinSideEffectExtensions
+{
+    /// <summary>
+    /// Side effect that provides a way for capsules to contain some state.
+    /// Similar to the `useState` hook from React;
+    /// see https://react.dev/reference/react/useState.
+    /// </summary>
+    public static (T, Action<T>) State<T>(
+        this ISideEffectRegistrar registrar,
+        T initial) =>
+        (default, _ => { });
+}
--- tests/Rearch.Tests/BasicTest.cs
+++ tests/Rearch.Tests/BasicTest.cs
@@ -22,4 +22,55 @@ public class BasicTest
         Assert.Equal(0, container.Read(Count));
         Assert.Equal(1, container.Read(CountPlusOne));
     }
+
+    [Fact]
+    public void StateUpdatesForStatefulCapsule()
+    {
+        (int, Action<int>) Stateful(ICapsuleHandle use) => use.State(0);
+        int PlusOne(ICapsuleHandle use) => use.Call(Stateful).Item1 + 1;
+
+        using var container = new Container();
+
+        {
+            var (state, setState) = container.Read(Stateful);
+            Assert.Equal(0, state);
+            setState(1);
+        }
+
+        {
+            var (state, setState) = container.Read(Stateful);
+            Assert.Equal(1, state);
+            setState(2);
+            setState(3);
+        }
+
+        {
+            var (state, _) = container.Read(Stateful);
+            Assert.Equal(3, state);
+        }
+    }
+
+    [Fact]
+    public void StateUpdatesForDependentCapsule()
+    {
+        (int, Action<int>) Stateful(ICapsuleHandle use) => use.State(0);
+        int PlusOne(ICapsuleHandle use) => use.Call(Stateful).Item1 + 1;
+
+        using var container = new Container();
+
+        {
+            var (state, setState) = container.Read(Stateful);
+            var statefulPlusOne = container.Read(PlusOne);
+            Assert.Equal(0, state);
+            Assert.Equal(1, statefulPlusOne);
+            setState(1);
+        }
+
+        {
+            var (state, _) = container.Read(Stateful);
+            var statefulPlusOne = container.Read(PlusOne);
+            Assert.Equal(1, state);
+            Assert.Equal(2, statefulPlusOne);
+        }
+    }
 }
