a59d74777b5b716372b09b7db1429d82bd7e3e0b
Nathaniel Bond <nabond251@gmail.com>
2024-01-13 19:03:45 -0500

    Add remaining side effects

--- src/Rearch/SideEffects.cs
+++ src/Rearch/SideEffects.cs
@@ -7,6 +7,29 @@ namespace Rearch;
 public static class BuiltinSideEffectExtensions
 {
     /// <summary>
+    /// Convenience side effect that gives a copy of the
+    /// <see cref="ISideEffectApi"/>.
+    /// </summary>
+    public static ISideEffectApi Api(this ISideEffectRegistrar registrar) =>
+        registrar.Register((api) => api);
+
+    /// <summary>
+    /// Convenience side effect that gives a copy of
+    /// <see cref="ISideEffectApi.Rebuild"/>.
+    /// </summary>
+    public static Action Rebuilder(this ISideEffectRegistrar registrar) =>
+        registrar.Api().Rebuild;
+
+    /// <summary>
+    /// Side effect that calls the supplied <paramref name="callback"/> once,
+    /// on the first build.
+    /// </summary>
+    public static T Callonce<T>(
+        this ISideEffectRegistrar registrar,
+        Func<T> callback) =>
+        registrar.Register((_) => callback());
+
+    /// <summary>
     /// Side effect that provides a way for capsules to contain some state,
     /// where the initial state is computationally expensive.
     /// Similar to the `useState` hook from React;
@@ -42,4 +65,28 @@ public static class BuiltinSideEffectExtensions
         this ISideEffectRegistrar registrar,
         T initial) =>
         registrar.LazyState(() => initial);
+
+    /// <summary>
+    /// Side effect that provides a way for capsules to hold onto some value
+    /// between builds, where the initial value is computationally expensive.
+    /// Similar to the `useRef` hook from React;
+    /// see https://react.dev/reference/react/useRef.
+    /// </summary>
+    public static T LazyValue<T>(
+        this ISideEffectRegistrar registrar,
+        Func<T> init) =>
+        registrar.Callonce(init);
+
+    /// <summary>
+    /// Side effect that provides a way for capsules to hold onto some value
+    /// between builds.
+    /// Similar to the `useRef` hook from React;
+    /// see https://react.dev/reference/react/useRef.
+    /// </summary>
+    public static T Value<T>(
+        this ISideEffectRegistrar registrar,
+        T initial) =>
+        registrar.LazyValue(() => initial);
+
+    // TODO: other side effects
 }
