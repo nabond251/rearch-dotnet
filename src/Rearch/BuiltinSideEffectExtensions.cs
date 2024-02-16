2a0e81f5f77fcfc609ae33a4193f40feca8f5bdf
Nathaniel Bond <nabond251@gmail.com>
2024-02-16 15:53:10 -0500

    Add reducer

--- src/Rearch/BuiltinSideEffectExtensions.cs
+++ src/Rearch/BuiltinSideEffectExtensions.cs
@@ -5,6 +5,18 @@
 namespace Rearch;
 
 /// <summary>
+/// A reducer function that consumes some <typeparamref name="TState"/> and
+/// <typeparamref name="TAction"/> and returns a new, transformed
+/// <typeparamref name="TState"/>.
+/// </summary>
+/// <typeparam name="TState">Type of state to consume.</typeparam>
+/// <typeparam name="TAction">Type of action to consume.</typeparam>
+/// <param name="state">State to consume.</param>
+/// <param name="action">Action to consume.</param>
+/// <returns>Reduced state.</returns>
+public delegate TState Reducer<TState, in TAction>(TState state, TAction action);
+
+/// <summary>
 /// A collection of builtin side effects.
 /// </summary>
 public static class BuiltinSideEffectExtensions
@@ -61,7 +73,7 @@ public static class BuiltinSideEffectExtensions
         this ISideEffectRegistrar registrar,
         Func<T>? init = null)
     {
-        return registrar.Register<(Func<T>, Action<T>)>(api =>
+        return registrar.InvokeOnce<(Func<T>, Action<T>)>(() =>
         {
             T state = default!;
             if (init != null)
@@ -235,6 +247,30 @@ public static class BuiltinSideEffectExtensions
             getDispose()?.Invoke();
             setDispose(effect());
         }
+    }
+
+    /// <summary>
+    /// A simple implementation of the reducer pattern as a side effect.
+    /// <br />
+    /// <br />
+    /// The React docs do a great job of explaining the reducer pattern more.
+    /// See https://react.dev/reference/react/useReducer.
+    /// </summary>
+    /// <typeparam name="TState">Type of state to consume.</typeparam>
+    /// <typeparam name="TAction">Type of action to consume.</typeparam>
+    /// <param name="registrar">Side effect registrar.</param>
+    /// <param name="reducer">Reducer function.</param>
+    /// <param name="initialState">Initial reducer state.</param>
+    /// <returns>Reducer state and action dispatcher.</returns>
+    public static (TState State, Action<TAction> Dispatch) Reducer<TState, TAction>(
+        this ISideEffectRegistrar registrar,
+        Reducer<TState, TAction> reducer,
+        TState initialState)
+    {
+        var (currState, setState) = registrar.State(initialState);
+        return (
+            currState,
+            (action) => setState(reducer(currState, action)));
     }
 
     // TODO(nabond251): other side effects
