56c4bc1e58181a4c93c189f8018ef577ecd9284a
Nathaniel Bond <nabond251@gmail.com>
2024-03-06 20:59:58 -0500

    Simulate load delay

--- examples/Rearch.Reactor.Example/Pages/MainPage.cs
+++ examples/Rearch.Reactor.Example/Pages/MainPage.cs
@@ -23,8 +23,11 @@ public static class ServiceHelper
 
 public static class DataAccess
 {
-    internal static async Task<IModelContext> ContextAsyncCapsule(ICapsuleHandle use) =>
-        (await ServiceHelper.ServicesAsync).GetRequiredService<IModelContext>();
+    internal static async Task<IModelContext> ContextAsyncCapsule(ICapsuleHandle use)
+    {
+        await Task.Delay(1000);
+        return (await ServiceHelper.ServicesAsync).GetRequiredService<IModelContext>();
+    }
 
     internal static AsyncValue<IModelContext> ContextWarmUpCapsule(ICapsuleHandle use)
     {
