e58f2324ac0a08a97e7b5eeca20c7ee4ff7736e0
Nathaniel Bond <nabond251@gmail.com>
2024-03-23 15:03:34 -0400

    Improve todo item

--- examples/Rearch.Reactor.Example/Components/TodoItem.cs
+++ examples/Rearch.Reactor.Example/Components/TodoItem.cs
@@ -2,18 +2,34 @@
 using System;
 using Rearch.Reactor.Example.Models;
 using Rearch.Reactor.Components;
+using Microsoft.Maui.Controls;
 
 namespace Rearch.Reactor.Example.Components;
 
 internal class TodoItem(Todo item, Action<Todo, bool> onItemDoneChanged) : CapsuleConsumer
 {
-    public override Grid Render(ICapsuleHandle use)
-        => Grid("54", "Auto, *",
-            CheckBox()
-                .IsChecked(item.Completed)
-                .OnCheckedChanged((s, args) => onItemDoneChanged(item, args.Value)),
-            Label(item.Title)
-                .TextDecorations(item.Completed ? TextDecorations.Strikethrough : TextDecorations.None)
-                .VCenter()
-                .GridColumn(1));
+    public override VisualNode Render(ICapsuleHandle use) =>
+        Frame(
+            Grid("27, 27", "Auto, *",
+                CheckBox()
+                    .IsChecked(item.Completed)
+                    .OnCheckedChanged((s, args) => onItemDoneChanged(item, args.Value))
+                    .GridRowSpan(2),
+                Label(item.Title)
+                    .FormattedText(() =>
+                    {
+                        FormattedString formattedString = new FormattedString();
+                        formattedString.Spans.Add(new Span
+                        {
+                            Text = item.Title,
+                            FontAttributes = FontAttributes.Bold,
+                        });
+                        return formattedString;
+                    })
+                    .VCenter()
+                    .GridColumn(1),
+                Label(item.Description)
+                    .VCenter()
+                    .GridRow(1)
+                    .GridColumn(1)));
 }
